d21d3a7c7886fee7c9059c0e0e8b2c7a2c0825dc
yota-hada <hadayota33@gmail.com>
2021-04-01 18:08:26 +0900

    :pencil2: Add opacity type.

--- src/types/tailwind.ts
+++ src/types/tailwind.ts
@@ -2,3 +2,4 @@ import config from '../../tailwind.config'
 
 export type FontSize = keyof typeof config.theme.fontSize
 export type Color = keyof typeof config.theme.colors
+export type Opacity = keyof typeof config.theme.opacity
